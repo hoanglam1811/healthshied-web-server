9b4c5d610f40f2f435c3e6dfe2c77899646b4b1b
hoanglam1811 <nguyenhoanglam18112003@gmail.com>
2025-03-12 20:00:07 +0700

    payment + notification db

---
+++ HealthShield/HealthShield.Services.Payment/PaymentService/Entities/Notification.cs
@@ -0,0 +1,11 @@
+﻿namespace PaymentService.Entities
+{
+	public class Notification
+	{
+		public int Id { get; set; }
+		public int UserId { get; set; }
+		public string Message { get; set; }
+		public DateTime SentAt { get; set; }
+		public string Status { get; set; }
+	}
+}
---
+++ HealthShield/HealthShield.Services.Payment/PaymentService/Migrations/20250312125810_noti.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PaymentService.Migrations
+{
+    /// <inheritdoc />
+    public partial class noti : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notifications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    Message = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    SentAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    Status = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notifications", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notifications");
+        }
+    }
+}
