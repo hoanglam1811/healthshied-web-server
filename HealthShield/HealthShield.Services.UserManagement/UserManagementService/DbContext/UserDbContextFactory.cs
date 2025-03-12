914e77984b603d8507dc7553ce79a6c668b61566
hoanglam1811 <tridmse173029@fpt.edu.vn>
2025-03-12 15:26:36 +0700

    fix user service

--- HealthShield/HealthShield.Services.UserManagement/UserManagementService/DbContext/UserDbContextFactory.cs
+++ HealthShield/HealthShield.Services.UserManagement/UserManagementService/DbContext/UserDbContextFactory.cs
@@ -1,27 +1,24 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.Extensions.Configuration;
-using System.IO;
-using UserManagementService.DbContexts;
 
 namespace UserManagementService.DbContexts
 {
-	public class UserDbContextFactory : IDesignTimeDbContextFactory<UserDbContext>
-	{
-		public UserDbContext CreateDbContext(string[] args)
-		{
-			// Load cấu hình từ appsettings.json
-			var config = new ConfigurationBuilder()
-				.SetBasePath(Directory.GetCurrentDirectory())
-				.AddJsonFile("appsettings.json", optional: false)
-				.Build();
+    public class UserDbContextFactory : IDesignTimeDbContextFactory<UserDbContext>
+    {
+        public UserDbContext CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
 
-			var optionsBuilder = new DbContextOptionsBuilder<UserDbContext>();
-			var connectionString = config.GetConnectionString("DefaultConnection");
+            var optionsBuilder = new DbContextOptionsBuilder<UserDbContext>();
+            var connectionString = config.GetConnectionString("DefaultConnection");
 
-			optionsBuilder.UseMySQL(connectionString);
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
 
-			return new UserDbContext(optionsBuilder.Options);
-		}
-	}
+            return new UserDbContext(optionsBuilder.Options);
+        }
+    }
 }
+
---
+++ HealthShield/HealthShield.Services.Vaccination/VaccinationService/DbContext/VaccinationDbContext.cs
@@ -0,0 +1,22 @@
+// using Microsoft.EntityFrameworkCore;
+// using UserManagementService.Entities;
+
+
+// namespace UserManagementService.DbContexts
+// {
+// 	public class UserDbContext : DbContext
+// 	{
+// 		public DbSet<User> Users { get; set; }
+// 		public DbSet<Child> Children { get; set; }
+
+// 		public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) { }
+
+// 		protected override void OnModelCreating(ModelBuilder modelBuilder)
+// 		{
+// 			modelBuilder.Entity<User>()
+// 				.HasMany(u => u.Children)
+// 				.WithOne(c => c.User)
+// 				.HasForeignKey(c => c.UserId);
+// 		}
+// 	}
+// }
